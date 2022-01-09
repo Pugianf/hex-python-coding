903442aac18f314a94516aa5b937371829ceeb60
Pugianf <pugianf@gmail.com>
2022-01-08 23:16:45 -0300

    update

--- 0x02-python-data_structures/1-prin_matrix_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        cont = 0
-        for elem in row:
-            print("{:d}".format(elem), end=(" "))
-            cont += 1
-        print()
