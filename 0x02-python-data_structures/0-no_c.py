58b723c5ed697172fd6135c80099af1982c8ec9b
Pugianf <pugianf@gmail.com>
2022-01-08 22:35:35 -0300

    update

--- 0x02-python-data_structures/0-no_c.py
+++ 0x02-python-data_structures/0-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = []
+    a = []
     for i in my_string:
         if (i != 'c') and (i != 'C'):
-            new_string.append(i)
+            a.append(i)
+    new_string = ''.join(a)
     return new_string
