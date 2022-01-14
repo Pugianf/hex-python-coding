ca0f275069b1c22f9cc993a3b1a7ff6d68f684c9
Pugianf <pugianf@gmail.com>
2022-01-13 23:16:01 -0300

    update

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -7,15 +7,15 @@ class Square:
     def __init__(self, size=0):
         """initializes attributes
         Args:
-            size (int): value to initialize `size`
+            size (int): size of the square
         Note:
-            ``Args`` section don't include `self` parameter.
+            ``Args`` section don't include `self` parameter
         Raises:
             TypeError: if `size` isn't an integer
             ValueError: if `size` is less than 0
-       """
-       if not isinstance(size, int):
-           raise TypeError('size must be an integer')
-       if size < 0:
-           raise ValueError('size must be >= 0')
-       self.size = size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
