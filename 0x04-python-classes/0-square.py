f4e6c4db9493fc02d98fc336f09e2a307bc6a96a
Pugianf <pugianf@gmail.com>
2022-01-13 21:49:00 -0300

    update

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -5,7 +5,9 @@
 class Square:
     """  Square is a class of squares """
 
+
     def __init__(self, size):
     """ function to initialize the private attribute size """
+
 
         self.__size = size
