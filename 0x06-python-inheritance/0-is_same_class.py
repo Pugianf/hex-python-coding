af46a06224d1178d4066f1e40f6e428a7384ba3c
Pugianf <pugianf@gmail.com>
2022-01-15 07:25:29 -0300

    update

--- 0x06-python-inheritance/0-is_same_class.py
+++ 0x06-python-inheritance/0-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""function that returns if is instance"""
 def is_same_class(obj, a_class):
+    """Returns: True if instance, False otherwise"""
     if isinstance(obj, a_class):
         return True
     return False
