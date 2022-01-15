add3f8f64ff0a1f1cc1659732165183b2f987dfe
Pugianf <pugianf@gmail.com>
2022-01-15 07:54:02 -0300

    update

---
+++ 0x06-python-inheritance/4-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creates a class base_geometry"""
+
+
+class BaseGeometry():
+    """Start of new class"""
+    def area(self):
+        """raises an exception"""
+        raise Exception('area() is not implemented')
