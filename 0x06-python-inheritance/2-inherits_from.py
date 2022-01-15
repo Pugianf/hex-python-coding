10f261291318281996e0d52d71b518a76e62452d
Pugianf <pugianf@gmail.com>
2022-01-15 07:44:49 -0300

    update

---
+++ 0x06-python-inheritance/2-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for the function: inherits from"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if derives of a class"""
+    if issubclass(type(obj), a_class):
+        return True
+    return False
