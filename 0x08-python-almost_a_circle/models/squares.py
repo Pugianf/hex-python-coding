cf33b6bdbaf2ddffecda75954e5bad76cce1330e
Pugianf <pugianf@gmail.com>
2022-01-17 12:04:50 -0300

    update

---
+++ 0x08-python-almost_a_circle/models/squares.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""import class Rectangle"""
+
+from models.rectangle import Rectangle
+
+"""Defines class Square that inherits from Rectangle"""
+
+
+class Square(Rectangle):
+    """Definition of own methods and attributes"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a square object"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return(
+            "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.size
+            )
+        )
