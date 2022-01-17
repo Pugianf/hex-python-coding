f234c052546406e128e8df4e412b8de6ce927cc7
Pugianf <pugianf@gmail.com>
2022-01-17 11:49:37 -0300

    update

--- 0x08-python-almost_a_circle/models/rectangle.py
+++ 0x08-python-almost_a_circle/models/rectangle.py
@@ -11,26 +11,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """initialize attributes of rectangle class"""
         super().__init__(id) 
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        if type(x) != int:
-            raise TypeError("x must be an integer")
-        if x <= 0:
-            raise ValueError("x must be > 0")
-        if type(y) != int:
-            raise TypeError("y must be an integer")
-        if y <= 0:
-            raise ValueError("y must be > 0")
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -41,7 +25,7 @@ class Rectangle(Base):
     def width(self, value):
         """ Setter for width """
         if type(value) != int:
-            raise ValueError("width must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
