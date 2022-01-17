8a3be0c67e89e31e8979c6c709d06dcdc195197b
Pugianf <pugianf@gmail.com>
2022-01-17 10:25:27 -0300

    update

--- 0x08-python-almost_a_circle/models/rectangle.py
+++ 0x08-python-almost_a_circle/models/rectangle.py
@@ -5,12 +5,13 @@ from models.base import Base
 
 """Defines class rectangle that inherite from Base"""
 
+
 class Rectangle(Base):
     """defines rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """initialize attributes of rectangle class"""
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
